5dbff18a2e89987bca434b3ecacf20cb2f6661c6
sumitDev12 <malviyasumit7987@gmail.com>
2026-01-28 23:41:04 +0000

    fix: disable CI lint errors

--- src/containers/talks/Talks.js
+++ src/containers/talks/Talks.js
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
 import "./Talks.scss";
 import TalkCard from "../../components/talkCard/TalkCard";
-import {talkSection} from "../../portfolio";
+import { talkSection } from "../../portfolio";
 import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
